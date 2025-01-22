e4cffb49dfb65ad231833d6ca8d3ebc50cd91df3
Idriss-rayan <rayanidriss27@gmail.com>
2025-01-22 18:12:07 +0600

    new add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/auth_gate.dart';
-import 'package:flutter_application_1/auth/login_or_register.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/themes/light_mode.dart';
 
