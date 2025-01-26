66b2e542ea64acc4eb00d1c9a590ab859a51b53c
Idriss-rayan <rayanidriss27@gmail.com>
2025-01-26 21:04:28 +0600

    add new things

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/auth_gate.dart';
-import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/themes/light_mode.dart';
 
 class MyApp extends StatelessWidget {
@@ -19,6 +18,13 @@ class MyApp extends StatelessWidget {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDt9U3iGiJcjjD3ptxurkSV7B5kHVX73JA",
+      appId: "1:285966935804:web:701abb0668a6a6e3fcd979",
+      messagingSenderId: "285966935804",
+      projectId: 'rayan123',
+    ),
+  );
   runApp(const MyApp());
 }
