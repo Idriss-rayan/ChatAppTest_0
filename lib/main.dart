f89f96bbd5d8a8371bfc5339ba99e655a8662444
Idriss-rayan <rayanidriss27@gmail.com>
2025-02-02 22:31:04 +0600

    i commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/auth_gate.dart';
 import 'package:flutter_application_1/themes/light_mode.dart';
